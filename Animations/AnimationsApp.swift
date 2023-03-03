6e46535055ccfcba0edafa4c474f5aac9db09d2d
Brandon Coston <costonb@gmail.com>
2023-03-02 22:17:17 -0800

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Brandon Coston on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
